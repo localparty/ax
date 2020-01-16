0ddded5f8043fb57bebf21c9eeb3e38da3eeb76b
Gee C <localparty@gmail.com>
2020-01-16 00:53:13 -0800

    Initial Commit

---
+++ accelerometer2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  accelerometer2
+//
+//  Created by Gee C on 1/16/20.
+//  Copyright © 2020 Gee C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
