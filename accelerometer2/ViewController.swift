73a866b65d20a4b4c19c5db4f27433b7eb69108b
Gee C <localparty@gmail.com>
2020-01-16 01:51:10 -0800

    first commit

--- accelerometer2/ViewController.swift
+++ accelerometer2/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    func getAccelerometerData(timer: Timer){
+        // Get the accelerometer data.
+        if let data = self.motion.accelerometerData {/*
+        if self.lastReading == nil {
+            self.lastReading = data
+            return
+        }*/
+            guard let lastReading = self.lastReading else {
+                self.lastReading = data
+                return
+            }
+            let dx = lastReading.acceleration.x - data.acceleration.x
+            let dy = lastReading.acceleration.x - data.acceleration.y
+            let dz = lastReading.acceleration.x - data.acceleration.z
+            print("dx: \(dx)\tdy: \(dy)\tdz:\(dz)");
+        }
+    }
+    let motion = CMMotionManager()
+    // i.e. 60Hz
+    let timerInterval:TimeInterval = 1.0 / 60.0
+    var timer: Timer?
+    var lastReading: CMAccelerometerData?
+    
+    @IBAction
+    func startAccelerometers(_ sender: UIButton) {
+       // testing for the accelerometer hardware availability
+       if self.motion.isAccelerometerAvailable {
+        self.motion.accelerometerUpdateInterval = self.timerInterval
+          self.motion.startAccelerometerUpdates()
 
+          // configuring a timer to fetch the data
+          self.timer = Timer(
+            fire: Date(),
+            interval: self.timerInterval,
+            repeats: true, block: self.getAccelerometerData)
+
+        // adding the timer to the current run loop
+        RunLoop.current.add(self.timer!, forMode: .default)
+       }
+    }
+    
+    @IBAction
+    func stopAccelerometer(_ sender: UIButton){
+        timer?.invalidate();
+    }
 
 }
 
